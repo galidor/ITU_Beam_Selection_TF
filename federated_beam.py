e904e018d2707c20ceb1ae6e877449030f14bee6
Mahdi Boloursaz Mashhadi <m.boloursaz-mashhadi@imperial.ac.uk>
2021-02-04 16:33:21 +0000

    v0.8

--- federated_beam.py
+++ federated_beam.py
@@ -1,4 +1,6 @@
 import collections
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import numpy as np
 import tensorflow as tf
@@ -16,9 +18,9 @@ from models import Lidar2D
 
 ################################Arguments################################
 MONTECARLO = 1
-NUM_VEHICLES = 2
+NUM_VEHICLES = 10
 LOCAL_EPOCHS = 1
-AGGREGATION_ROUNDS = 30
+AGGREGATION_ROUNDS = 100
 
 BATCH_SIZE = 16
 SHUFFLE_BUFFER = 20
@@ -36,12 +38,12 @@ def get_local_dataset(lidar_path, beam_path, num_vehicles, vehicle_ID):
     training_data = LidarDataset2D(lidar_path, beam_path)
     training_data.lidar_data = np.transpose(training_data.lidar_data, (0, 2, 3, 1))
     x=training_data.lidar_data
-    xx = x[vehicle_ID*int(x.shape[0]/num_vehicles):(vehicle_ID+1)*int(x.shape[0]/num_vehicles),:,:,:] ###Split Lidar Data
+    random_indices = np.random.choice(x.shape[0], size=int(x.shape[0]/num_vehicles), replace=False) 
+    xx = x[random_indices,:,:,:] ###Local LIDAR Data
     y = training_data.beam_output
-    yy = y[vehicle_ID*int(y.shape[0]/num_vehicles):(vehicle_ID+1)*int(y.shape[0]/num_vehicles),:] ###Split Beam Labels
-    
+    yy = y[random_indices,:] ###Local Beam Labels    
     dataset_train = tf.data.Dataset.from_tensor_slices((list(xx.astype(np.float32)),list(yy.astype(np.float32))))
-    #sio.savemat('label'+str(k)+'.mat',{'label'+str(k):yy})
+    sio.savemat('label'+str(vehicle_ID)+'.mat',{'label'+str(vehicle_ID):yy})
     return dataset_train
 
 def get_test_dataset(lidar_path, beam_path):
@@ -79,7 +81,6 @@ def create_keras_model():
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(16),
     tf.keras.layers.ReLU(),
-    # layers.Dropout(0.7),
     tf.keras.layers.Dense(256),
     tf.keras.layers.Softmax()])
 
@@ -100,8 +101,9 @@ def model_fn():
 ################################Main################################   
 iterative_process = tff.learning.build_federated_averaging_process(
     model_fn,
-    client_optimizer_fn=lambda: tf.keras.optimizers.Adam(lr=5e-3),
-    server_optimizer_fn=lambda: tf.keras.optimizers.SGD(lr=.25))
+    client_optimizer_fn=lambda: tf.keras.optimizers.SGD(lr=.2, decay=.001),
+    server_optimizer_fn=lambda: tf.keras.optimizers.SGD(lr=.2))
+
 
 evaluation = tff.learning.build_federated_evaluation(model_fn)
 
@@ -111,7 +113,8 @@ test_data.lidar_data = np.transpose(test_data.lidar_data, (0, 2, 3, 1))
 accFL=0  
 for MONTECARLOi in range(MONTECARLO):
     ###Generate Federated Train Dataset
-    federated_train_data=[]     
+    federated_train_data=[]
+    federated_test_data=[]
     for i in range(NUM_VEHICLES):
         train_dataset = get_local_dataset(lidar_training_path, beam_training_path,NUM_VEHICLES,i)
         federated_train_data.append(preprocess(train_dataset))
@@ -125,40 +128,38 @@ for MONTECARLOi in range(MONTECARLO):
     
     state = iterative_process.initialize() ###Initialize training
     
-    #top1 = tf.keras.metrics.TopKCategoricalAccuracy(k=1, name='top_1_categorical_accuracy', dtype=None)
-    #top10 = tf.keras.metrics.TopKCategoricalAccuracy(k=10, name='top_10_categorical_accuracy', dtype=None)
-    
     ###Federated Training    
     for round_num in range(AGGREGATION_ROUNDS):
       state, metrics = iterative_process.next(state, federated_train_data)
       test_metrics = evaluation(state.model, federated_test_data)
-      
       print(str(metrics))
       print(str(test_metrics))
       
       top1[round_num]=test_metrics['top_1_categorical_accuracy']
       top10[round_num]=test_metrics['top_10_categorical_accuracy']
     
-      ###Generate Accuracy and Throughput Performance Curves
-      keras_model = create_keras_model()
-      #keras_model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), metrics=[top1,top10])
-      state.model.assign_weights_to(keras_model)
-      test_preds = keras_model.predict(test_data.lidar_data, batch_size=100)
-      test_preds_idx = np.argsort(test_preds, axis=1)
-      top_k = np.zeros(100)
-      throughput_ratio_at_k = np.zeros(100)
-      correct = 0
-      for i in range(100):
-        correct += np.sum(test_preds_idx[:, -1-i] == np.argmax(test_data.beam_output, axis=1))
-        top_k[i] = correct/test_data.beam_output.shape[0]
-        throughput_ratio_at_k[i] = np.sum(np.log2(np.max(np.take_along_axis(test_data.beam_output_true, test_preds_idx, axis=1)[:, -1-i:], axis=1) + 1.0))/\
+    
+    ###Generate Accuracy and Throughput Performance Curves
+    keras_model = create_keras_model()
+    keras_model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), metrics=[top1,top10])
+    state.model.assign_weights_to(keras_model)
+    test_preds = keras_model.predict(test_data.lidar_data, batch_size=100)    
+    test_preds_idx = np.argsort(test_preds, axis=1)
+    top_k = np.zeros(100)
+    throughput_ratio_at_k = np.zeros(100)
+    correct = 0
+    for i in range(100):
+      correct += np.sum(test_preds_idx[:, -1-i] == np.argmax(test_data.beam_output, axis=1))
+      top_k[i] = correct/test_data.beam_output.shape[0]
+      throughput_ratio_at_k[i] = np.sum(np.log2(np.max(np.take_along_axis(test_data.beam_output_true, test_preds_idx, axis=1)[:, -1-i:], axis=1) + 1.0))/\
                                    np.sum(np.log2(np.max(test_data.beam_output_true, axis=1) + 1.0))
         
-      sio.savemat('federated_accuracy'+str(round_num)+'.mat',{'accuracy':top_k})
-      sio.savemat('federated_throughput'+str(round_num)+'.mat',{'throughput':throughput_ratio_at_k})
-    
-    sio.savemat('top1.mat',{'top1':top1})
-    sio.savemat('top10.mat',{'top10':top10})
+      
+    sio.savemat('top1_V'+str(NUM_VEHICLES)+'.mat',{'top1':top1})
+    sio.savemat('top10_V'+str(NUM_VEHICLES)+'.mat',{'top10':top10})
+    sio.savemat('federated_accuracy_V'+str(NUM_VEHICLES)+'.mat',{'accuracy':top_k})
+    sio.savemat('federated_throughput_V'+str(NUM_VEHICLES)+'.mat',{'throughput':throughput_ratio_at_k})
+        
     
     np.savez("federated.npz", classification=top_k, throughput_ratio=throughput_ratio_at_k)
     accFL=accFL+metrics['train']['top_10_categorical_accuracy']/MONTECARLO
